f7c31c2623f2e1cb5159940d6953172402937d1c
Priyansh Jhalora <2020kucp1038@iiitkota.ac.in>
2022-06-07 18:53:14 +0530

    previous skeleton commit v2.0 --priyansh

--- src/components/JoinQuiz.js
+++ src/components/JoinQuiz.js
@@ -1,14 +1,31 @@
+import "./joinQuiz.css";
 import React from "react";
 
 const JoinQuiz = () => {
   return (
     <>
-      <div id="question">this is a question ?</div>
+      <div id="question">
+        <p>
+          this is a
+          questionsf,jvhlsdhbvsdbvvdsbkjkkkkkkkjk.,dbfkjbsjvb;slkkv;jsdbvvlkDAkvjbsbdk;vjvbsdvbdvsdjjlsd
+          kj
+          dddddddddddddddddddddddddddddddddddddsdbvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvkajghjh
+          oihwroigh ?.sjbvkja KJDbcADJkvjcbaSLC dklkva l/avaocad ak c dkc
+        </p>
+      </div>
       <div id="options">
-        <div className="option">A</div>
-        <div className="option">B</div>
-        <div className="option">C</div>
-        <div className="option">D</div>
+        <div className="option">
+          <p>Asmdhvkhsdbvbk.bsdkj/bk/sdv kjbkjgksbkvbvs</p>
+        </div>
+        <div className="option">
+          <p>Bakdfhglisksb;vgiugiug foug foff sfuish/f</p>
+        </div>
+        <div className="option">
+          <p>cSBMBDk. vkjbaDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDbcl</p>
+        </div>
+        <div className="option">
+          <p>Dkrrhgiubskviujbks;9gr owrhhow</p>
+        </div>
       </div>
     </>
   );
