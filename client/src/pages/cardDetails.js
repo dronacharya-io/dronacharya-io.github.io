4ada8e01573739b996d8083b3c1768aec8e81972
Aman Tank <69970589+amantank@users.noreply.github.com>
2022-06-25 14:32:47 +0530

    kuch bhi--aman

--- client/src/pages/cardDetails.js
+++ client/src/pages/cardDetails.js
@@ -1,4 +1,5 @@
 const Details = [
+<<<<<<< Updated upstream
   {
     id:1,
     imgUrl: "",
@@ -14,6 +15,20 @@ const Details = [
     imgUrl: "",
     topic: "3",
   },
+=======
+    {
+        imgUrl : "",
+        topic : "1"
+    },
+    {
+        imgUrl : "",
+        topic : "2"
+    },
+    {
+        imgUrl : "",
+        topic : "3"
+    }
+>>>>>>> Stashed changes
 ];
 
 export default Details;
