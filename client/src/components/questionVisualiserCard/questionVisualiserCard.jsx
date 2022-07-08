bd15eb82637d01ac31f6a2fb888385489fe86253
Priyansh Jhalora <2020kucp1038@iiitkota.ac.in>
2022-07-08 18:46:54 +0530

    update added remove function

--- client/src/components/questionVisualiserCard/questionVisualiserCard.jsx
+++ client/src/components/questionVisualiserCard/questionVisualiserCard.jsx
@@ -1,17 +1,20 @@
-const QuestionVisualiserCard = (question) => {
-  const { Question, options, correctAns } = question;
+const QuestionVisualiserCard = (props) => {
+  const { Question, options, correctAns } = props.question;
   return (
-    <div className="item">
-      <h4>{Question}</h4>
-      {options.map((option) => {
-        return (
-          <>
-            <p>{option}</p>
-          </>
-        );
-      })}
-      <p>{correctAns}</p>
-    </div>
+    <>
+      <div className="item">
+        <h4>{Question}</h4>
+        {options.map((option) => {
+          return (
+            <>
+              <p>{option}</p>
+            </>
+          );
+        })}
+        <p>{correctAns}</p>
+      </div>
+      <button id="removeQuestion">-</button>
+    </>
   );
 };
 
