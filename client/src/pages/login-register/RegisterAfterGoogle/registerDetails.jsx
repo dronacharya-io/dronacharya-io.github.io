d137b0ea925afe1202fe73895228f7117f12839c
Aman Tank <69970589+amantank@users.noreply.github.com>
2022-07-12 16:04:55 +0530

    reRegister added -- aman

---
+++ client/src/pages/login-register/RegisterAfterGoogle/registerDetails.jsx
@@ -0,0 +1,35 @@
+import React from "react"
+
+function RegisterDetails(props){
+
+    return(
+        <>
+            <form>
+                <div>
+                    <input 
+                    type="text" 
+                    placeholder="Full Name" />
+                </div>
+                <div>    
+                    <input 
+                    type="email" 
+                    placeholder="email"
+                    value={/*props.googleEmail*/}
+                    readOnly
+                    />
+                </div>
+                <div>    
+                    <input 
+                    type="password" 
+                    placeholder="password"  />
+                </div>
+                <div>
+                    <input 
+                    type="password" 
+                    placeholder="password" />
+                </div>
+
+            </form>
+        </>
+    )
+}
