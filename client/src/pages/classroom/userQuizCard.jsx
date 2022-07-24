a55536b5f3039d4a10fca8681588be285e83a5f8
Aman Tank <69970589+amantank@users.noreply.github.com>
2022-07-24 14:50:05 +0530

    added functional cards -- aman

--- client/src/pages/classroom/userQuizCard.jsx
+++ client/src/pages/classroom/userQuizCard.jsx
@@ -1,18 +1,18 @@
 import React, { useState, useEffect } from "react";
 
 function UserQuizCard(props){
-    const [colour, setColour] = useState('white');
-    const [date, setDate] = useState(new Date().toLocaleDateString());
-
+    const [currentDate, setCurrentDate] = useState(new Date().getFullYear() + "-" +"0"+ (new Date().getMonth()+1)+"-"+ new Date().getDate() );
+    const [currentTime, setCurrentTime] = useState(new Date().getHours()+":"+new Date().getMinutes())
+    console.log(currentTime)
     useEffect(()=>{
         setInterval(()=>{
-            setDate(new Date().toLocaleDateString())
+            setCurrentDate(new Date().getFullYear() + "-" +"0"+ (new Date().getMonth()+1)+"-"+ new Date().getDate())
+            setCurrentTime(new Date().getHours()+":"+new Date().getMinutes())
         },1000)
     },[])
-    
 
     return(
-        <div className="card" style={{backgroundColor: colour }}>
+        <div className="card" style={{backgroundColor: props.startDate < currentDate || props.runTime < currentTime ? "#E0E0E0" : props.startDate > currentDate || props.currentTime > currentTime ? "#A4EBF3" : props.startDate === currentDate  ? "green" : "black" }}>
             <div style={{width : "15rem",height:"15rem"}}>
                 <img src="https://picsum.photos/seed/picsum/240/240" />
             </div>
@@ -20,7 +20,7 @@ function UserQuizCard(props){
                 <h4>{props.quizName}</h4>
                 <p style={{display:"inline-block", position:"relative", right:'55px',marginTop:'9px'}}>{props.startDate}</p>
                 <p style={{display:"inline-block", position:"relative", left:'55px',marginTop:'9px'}}>{props.runTime}</p>
-                <p>{date}</p>
+                <p>{currentDate}</p>
             </div>
         </div>
     )
