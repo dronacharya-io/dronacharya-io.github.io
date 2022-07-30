5f9284d490c89fd2822e537ed8ef339b2188141b
Aman Tank <69970589+amantank@users.noreply.github.com>
2022-07-30 17:31:57 +0530

    cards updated -- aman

--- client/src/components/cards/Cards.jsx
+++ client/src/components/cards/Cards.jsx
@@ -1,12 +1,36 @@
 import React from "react";
+import Cards from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
 
 export default function Card(props){
 
     return(
-        <div className="card">
-            <h5>{props.topic}</h5>
-            <img src={props.img} alt="UpcomingEvent" />
-            <button>click to know more..</button>
-        </div>
+        <>
+            <Cards sx={{ maxWidth: 345 }} className="card" >
+                <CardMedia 
+                    components="img"
+                    alt="quizImg"
+                    height="2400"
+                    image={props.img}
+                />
+                <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                    {props.topic}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                    {props.startDate}
+                </Typography>
+                </CardContent>
+                <CardActions>
+                    <Button size="small">Share</Button>
+                    <Button size="small">Learn More</Button>
+                </CardActions>
+            </Cards>
+        </>
     );
 }
--- client/src/pages/classroom/userQuizCard.jsx
+++ client/src/pages/classroom/userQuizCard.jsx
@@ -1,10 +1,18 @@
 import React, { useState, useEffect } from "react";
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
 
 function UserQuizCard(props){
     const [zero, setZero] = useState("0");
     const [currentDate, setCurrentDate] = useState(new Date().getFullYear() + "-" +zero+ (new Date().getMonth()+1)+"-"+ new Date().getDate() );
     const [currentTime, setCurrentTime] = useState(new Date().getHours()+":"+new Date().getMinutes())
-    
+    const tempImage = "https://picsum.photos/seed/picsum/240/240"; 
+
     useEffect(()=>{ 
         setInterval(()=>{
             if((new Date().getMonth()+1) > 9){
@@ -16,17 +24,31 @@ function UserQuizCard(props){
     },[])
 
     return(
-        <div className="card" style={{backgroundColor: props.startDate < currentDate && props.runTime < currentTime ? "#E0E0E0" : props.startDate > currentDate || props.runTime > currentTime ? "#A4EBF3" : props.startDate === currentDate && props.runTime < currentTime  ? "#E0E0E0" : "white" }}>
-            <div style={{width : "15rem",height:"15rem"}}>
-                <img src="https://picsum.photos/seed/picsum/240/240" />
-            </div>
-            <div>
-                <h4>{props.quizName}</h4>
-                <p style={{display:"inline-block", position:"relative", right:'55px',marginTop:'9px'}}>{props.startDate}</p>
-                <p style={{display:"inline-block", position:"relative", left:'55px',marginTop:'9px'}}>{props.runTime}</p>
-                <p>{currentDate}</p>
-            </div>
-        </div>
+       <>
+            <Card sx={{ maxWidth: 345 }} className="card" >
+                <CardMedia 
+                    components="img"
+                    alt="quizImg"
+                    height="2400"
+                    image={tempImage}
+                />
+                <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                    {props.quizName}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                    {props.startDate}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                    {props.runTime}
+                </Typography>
+                </CardContent>
+                <CardActions>
+                    <Button size="small">Share</Button>
+                    <Button size="small">Learn More</Button>
+                </CardActions>
+            </Card>
+        </>
     )
 }
 
