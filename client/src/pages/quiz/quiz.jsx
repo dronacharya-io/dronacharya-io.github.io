27491d6ee7bdbd2bff578a1c33a0c536dec58887
Priyansh Jhalora <2020kucp1038@iiitkota.ac.in>
2022-08-05 11:04:36 +0530

    animation added --priyansh

--- client/src/pages/quiz/quiz.jsx
+++ client/src/pages/quiz/quiz.jsx
@@ -6,7 +6,8 @@ import { useNavigate } from "react-router-dom";
 import Button from "@mui/material/Button";
 import AddBoxIcon from "@mui/icons-material/AddBox";
 import TextField from "@mui/material/TextField";
-import Input from "@mui/material/Input";
+import Lottie from "react-lottie";
+import animationData from "../../lotties/circle-animation.json";
 
 export const Quiz = () => {
   const { user } = useUserAuth();
@@ -18,82 +19,98 @@ export const Quiz = () => {
     setId(e.target.value);
   };
 
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+
   return (
-    <div className="body">
-      <ReactTypingEffect
-        className="typing"
-        text={[
-          user
-            ? "Namaste, " +
-              user.displayName?.slice(0, 1).toUpperCase() +
-              user.displayName?.slice(1, user.displayName?.length).toLowerCase()
-            : "Namaste",
-        ]}
-        cursorRenderer={(cursor) => <h1>{cursor}</h1>}
-        speed={100}
-        eraseSpeed={100}
-        displayTextRenderer={(text, i) => {
-          return (
-            <h1>
-              {text
-                .split(" ")
-                .filter((char, i) => {
-                  return i < 2;
-                })
-                .map((char, i) => {
-                  const key = `${i}`;
-                  return (
-                    <span
-                      key={key}
-                      className={i === 0 ? "namaste" : ""}
-                      style={
-                        i === 0
-                          ? {
-                              fontFamily: "Dancing Script",
-                            }
-                          : { fontFamily: "Dancing Script" }
-                      }
-                    >
-                      {char + " "}
-                    </span>
-                  );
-                })}
-            </h1>
-          );
-        }}
-      />
-      <div id="centerContent">
-        <div id="homePageText">
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type
-          PageMaker including versions of Lorem Ipsum.
-        </div>
-        <div id="q-mainBody">
-          <Button
-            variant="contained"
-            id="CreateQuizButton"
-            onClick={() => navigate("/createQuiz")}
-          >
-            <AddBoxIcon
-              style={{ position: "relative", right: "1rem", height: "28px" }}
+    <div className="bodyWithAnimation">
+      <div className="body">
+        <ReactTypingEffect
+          className="typing"
+          text={[
+            user
+              ? "Namaste, " +
+                user.displayName?.slice(0, 1).toUpperCase() +
+                user.displayName
+                  ?.slice(1, user.displayName?.length)
+                  .toLowerCase()
+              : "Namaste",
+          ]}
+          cursorRenderer={(cursor) => <h1>{cursor}</h1>}
+          speed={100}
+          eraseSpeed={100}
+          displayTextRenderer={(text, i) => {
+            return (
+              <h1>
+                {text
+                  .split(" ")
+                  .filter((char, i) => {
+                    return i < 2;
+                  })
+                  .map((char, i) => {
+                    const key = `${i}`;
+                    return (
+                      <span
+                        key={key}
+                        className={i === 0 ? "namaste" : ""}
+                        style={
+                          i === 0
+                            ? {
+                                fontFamily: "Dancing Script",
+                              }
+                            : { fontFamily: "Dancing Script" }
+                        }
+                      >
+                        {char + " "}
+                      </span>
+                    );
+                  })}
+              </h1>
+            );
+          }}
+        />
+        <div id="centerContent">
+          <div id="homePageText">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            PageMaker including versions of Lorem Ipsum.
+          </div>
+          <div id="q-mainBody">
+            <Button
+              variant="contained"
+              id="CreateQuizButton"
+              onClick={() => navigate("/createQuiz")}
+            >
+              <AddBoxIcon
+                style={{ position: "relative", right: "1rem", height: "28px" }}
+              />
+              Create Test
+            </Button>
+
+            <TextField
+              id="joinquiztextfield"
+              placeholder="Enter a code"
+              onChange={handleChange}
             />
-            Create Test
-          </Button>
 
-          <TextField
-            id="joinquiztextfield"
-            placeholder="Enter a code"
-            onChange={handleChange}
-          />
-
-          <Button
-            id="joinquizbutton"
-            onClick={() => navigate("/joinQuiz/?id=" + id)}
-          >
-            Join
-          </Button>
+            <Button
+              id="joinquizbutton"
+              onClick={() => navigate("/joinQuiz/?id=" + id)}
+            >
+              Join
+            </Button>
+          </div>
         </div>
+      </div>
+      <div id="animation">
+        <Lottie options={defaultOptions} height={700} width={700} />
       </div>
     </div>
   );
