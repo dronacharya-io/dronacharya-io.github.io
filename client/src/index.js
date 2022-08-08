6f1b3387f7fc6fd77528fb27863898c143b2caeb
Priyansh Jhalora <2020kucp1038@iiitkota.ac.in>
2022-08-08 16:42:54 +0530

    commit

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { UserAuthContextProvider } from "./context/AuthContext";
+import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <UserAuthContextProvider>
-      <App />
+      <HashRouter>
+        <App />
+      </HashRouter>
     </UserAuthContextProvider>
   </React.StrictMode>
 );
