34c07356bcc7a67c10e6121b1241105b2de37131
Aman Tank <69970589+amantank@users.noreply.github.com>
2022-08-08 15:47:04 +0530

    komit

--- client/src/components/navbar/navbar.jsx
+++ client/src/components/navbar/navbar.jsx
@@ -24,7 +24,7 @@ const Navbar = () => {
         <span id="logo">
           <img
             src={Logo}
-            style={{ width: "3rem", height: "3rem" }}
+            style={{ width: "3rem", height: "auto" }}
             onClick={() => {
               navigate("/");
             }}
