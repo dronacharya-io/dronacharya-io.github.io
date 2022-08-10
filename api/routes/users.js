a694f9d8100cc66dd3843d480d985147547af149
Priyansh Jhalora <2020kucp1038@iiitkota.ac.in>
2022-08-10 17:18:18 +0530

    commit

--- api/routes/users.js
+++ api/routes/users.js
@@ -4,7 +4,7 @@ import {
   deleteUser,
   getUser,
   getUsers,
-} from "../controllers/user.js";
+} from "../controllers/User.js";
 import { verifyAdmin, verifyUser } from "../utils/verifyToken.js";
 
 const router = express.Router();
