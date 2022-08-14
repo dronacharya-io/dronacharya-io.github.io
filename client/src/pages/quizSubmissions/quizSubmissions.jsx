59dc23c774f2e79fa6187d3299e451860651b1b6
Aman Tank <69970589+amantank@users.noreply.github.com>
2022-08-14 18:52:24 +0530

    added table ui to the quizsubmission -- aman

--- client/src/pages/quizSubmissions/quizSubmissions.jsx
+++ client/src/pages/quizSubmissions/quizSubmissions.jsx
@@ -1,12 +1,23 @@
-import React from "react";
-import { useEffect } from "react";
+import React,{ useEffect, useState } from "react";
 import axios from "axios";
-import { useState } from "react";
-import TableHead from "@mui/material/TableHead";
-import TableCard from "./QuizSubmisionTable/TableCard";
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import { useNavigate } from "react-router-dom";
+import Button from "@mui/material/Button";
+
+
 
 const QuizSubmissions = () => {
   const [data, setData] = useState();
+  
+
+
+  const navigate = useNavigate();
 
   const urlParams = new URLSearchParams(window.location.search);
   console.log(urlParams.get("id"));
@@ -19,6 +30,7 @@ const QuizSubmissions = () => {
         );
         console.log(res.data.attendies);
         setData(res.data.attendies);
+
         console.log(data);
       } catch (err) {
         console.log(err.message);
@@ -29,20 +41,54 @@ const QuizSubmissions = () => {
     };
   }, []);
 
+
+
+
   return (
     <>
-      {data?.map((attendie) => {
-        return (
-          <>
-            <TableCard
-              name={attendie.userName}
-              score={attendie.score}
-              email={attendie.userEmail}
-              userID={attendie.userId}
-            />
-          </>
-        );
-      })}
+      <>
+        <TableContainer component={Paper}>
+          <Table stickyHeader aria-label="sticky table" sx={{ minWidth: 650 }} >
+            <TableHead >
+              <TableRow>
+                <TableCell>Examinee</TableCell>
+                <TableCell align="right">ExaminEmail</TableCell>
+                <TableCell align="right">Examinee-score</TableCell>
+                <TableCell align="right">See Attemted Answers</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {data?.map((row) => (
+                <TableRow
+                  key={row.userName}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {row.userName}
+                  </TableCell>
+                  <TableCell align="right">{row.userEmail}</TableCell>
+                  <TableCell align="right">{row.score}</TableCell>
+                  <TableCell align='right'>
+                    <Button 
+                      id=""
+                      variant='outlined'
+                      onClick={() =>
+                        navigate(
+                          "../quizScore?quizId=" +
+                            urlParams.get("id") +
+                            "&userId=" +
+                            row.userId)
+                      }
+                    >
+                    Click
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </>
     </>
   );
 };
