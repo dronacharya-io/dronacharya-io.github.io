a7ab57366fc278e97018e85fa1071de54db61843
Aman Tank <132202130+amanntank@users.noreply.github.com>
2023-05-07 09:39:25 +0530

    c

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -4,7 +4,7 @@ export const updateUser = async (req, res, next) => {
   try {
     const updatedUser = await User.findByIdAndUpdate(
       req.params.id,
-      { $eq: req.body },
+      { $set: req.body },
       { new: true }
     );
     res.status(200).json(updatedUser);
